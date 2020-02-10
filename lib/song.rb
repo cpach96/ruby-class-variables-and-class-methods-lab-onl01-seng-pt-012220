01e1a05cb56a4a58d4ded75957654761e74425ba
Curtis pachtinger <cpach96@gmail.com>
2020-02-10 21:52:55 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+    def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq!
+  end
+  
+  def self.artists
+    @@artists.uniq!
+  end
+  
+  def self.genre_count
+    counter = 0 
+    genre_counter = {}
+    @@genres.each do |g|
+     genre_counter[g] = counter
+    end
+  end
+  genre_counter
+end
+end
